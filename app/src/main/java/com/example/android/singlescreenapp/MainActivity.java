67633361abac9c56d149060f5b265c9f3c839216
darksomehandsome <38291329+darksomehandsome@users.noreply.github.com>
2019-07-18 12:30:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.singlescreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void openAddress_onClick(View view) {
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=NAMOT 504 Main Street San Diego, California 92123"));
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+
+    public void makeCall_onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "619-543-1234", null));
+        startActivity(intent);
+    }
+
+    public void sendEmail_onClick(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:guest_relations@namot.org"));
+        emailIntent.putExtra(emailIntent.EXTRA_SUBJECT, "Queries");
+        emailIntent.putExtra(emailIntent.EXTRA_TEXT, "Hi,\nmy name is ");
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(emailIntent);
+
+        }
+    }
+
+    public void visitWebsite_onClick(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.www.namot.org")));
+    }
+
+}
